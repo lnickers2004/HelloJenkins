c57e4526eda978308e8a5b52294c28e069baf3de
lnickers2004 <lnickers@austin.rr.com>
2013-08-26 01:18:23 -0500

    initial commit for jenkins setup test

---
+++ HelloJenkins.Tests/and_and_comparing_two_numbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace HelloJenkins.Tests
+{
+    [TestFixture]
+    public class and_and_comparing_two_numbers
+    {
+        [Test]
+        public void and_comparing_7_to_5_test()
+        {
+            Assert.Greater(7, 5);
+        }
+    }
+}
