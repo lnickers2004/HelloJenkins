90a603db877f9a67e39c8b60dad4842a5bf7d3d7
Larry Nickerson <lnickers@austin.rr.com>
2013-08-28 02:46:48 -0500

    Added MSTest tests to see if Jenkins can handle both types

---
+++ MSTestsClassProject/when_testing_with_mstest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace HelloJenkins.MSTests
+{
+	[TestClass]
+	public class when_testing_with_mstest
+	{
+		[TestMethod]
+		public void then_running_this_test_should_pass()
+		{
+			Assert.AreEqual(true,true);
+		}
+	}
+
+
+}
